629fd0dabe08099bc34e180c1fa77982e6776ad0
damienmillet <contact@damien-millet.dev>
2022-09-26 20:04:41 +0200

    🍱 update entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -18,31 +18,31 @@ fi
 
 # if vpn isn't on, stop rtorrent if it's running and run vpn if file conf exist
 # if vpn is on, start rtorrent if it's not running
-# while true; do
-#     if ! pgrep openvpn > /dev/null; then
-#         if pgrep rtorrent > /dev/null; then
-#           echo "OpenVPN is not running, stopping rtorrent..."
-#           killall rtorrent
-#           sleep 5
-#         fi
-#         echo "OpenVPN is not running, restarting..."
-#         if [ -f /config/client.ovpn ]; then
-#             echo "Starting OpenVPN..."
-#             openvpn --config /config/client.ovpn --daemon
-#             sleep 5
-#         else
-#             echo "No OpenVPN configuration found in /config. Exiting..."
-#             exit 1
-#         fi
-#         sleep 5
-#     else 
-#         if ! pgrep rtorrent > /dev/null; then
-#           echo "OpenVPN is running, starting rtorrent..."
-#           sudo -S -u rtorrent  sudo -S -u rtorrent  rtorrent&
-#           sleep 5
-#         fi
-#     fi
-#     sleep 5
-# done
+while true; do
+    if [! pgrep openvpn > /dev/null || ifconfig | grep tun0 | wc -l ]; then
+        if pgrep rtorrent > /dev/null; then
+          echo "OpenVPN is not running, stopping rtorrent..."
+          killall rtorrent
+          sleep 5
+        fi
+        echo "OpenVPN is not running, restarting..."
+        if [ -f /config/client.ovpn ]; then
+            echo "Starting OpenVPN..."
+            openvpn --config /config/client.ovpn --daemon
+            sleep 5
+        else
+            echo "No OpenVPN configuration found in /config. Exiting..."
+            exit 1
+        fi
+        sleep 5
+    else 
+        if ! pgrep rtorrent > /dev/null; then
+          echo "OpenVPN is running, starting rtorrent..."
+          sudo -S -u rtorrent  sudo -S -u rtorrent  rtorrent&
+          sleep 5
+        fi
+    fi
+    sleep 5
+done
 
 sudo -S -u rtorrent flood --host 0.0.0.0
